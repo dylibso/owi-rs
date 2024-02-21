6cf963c9da88a96264dd18be4a965df7efa93c41
zach <zach@dylibso.com>
2024-02-21 12:51:04 -0800

    cleanup: drop the _symbol prefix on functions that return symbolic values

--- src/lib.rs
+++ src/lib.rs
@@ -14,33 +14,33 @@ pub mod sys {
 }
 
 macro_rules! wrap {
-    ($x:ident, ($($n:ident : $t:ty),*) $(-> $r:ty)?, $doc:expr) => {
+    ($a: ident, $x:ident, ($($n:ident : $t:ty),*) $(-> $r:ty)?, $doc:expr) => {
         #[doc=$doc]
-        pub fn $x($($n:$t),*) $(-> $r)? {
+        pub fn $a($($n:$t),*) $(-> $r)? {
             unsafe { sys::$x($($n),*) }
         }
     };
 }
 
-wrap!(assume, (x: bool), "Add an assumption to the verifier");
-wrap!(assert, (x: bool), "Add an assertion to the verifier");
-wrap!(i8_symbol, () -> i8, "Create an `i8` symbolic value");
-wrap!(i32_symbol, () -> i32, "Create an `i32` symbolic value");
-wrap!(i64_symbol, () -> i64, "Create an `i64` symbolc value");
-wrap!(f32_symbol, () -> f32, "Create an `f32` symbolic value");
-wrap!(f64_symbol, () -> f64, "Create an `f64` symbolic value");
+wrap!(assume, assume, (x: bool), "Add an assumption to the verifier");
+wrap!(assert, assert, (x: bool), "Add an assertion to the verifier");
+wrap!(i8, i8_symbol, () -> i8, "Create an `i8` symbolic value");
+wrap!(i32, i32_symbol, () -> i32, "Create an `i32` symbolic value");
+wrap!(i64, i64_symbol, () -> i64, "Create an `i64` symbolc value");
+wrap!(f32, f32_symbol, () -> f32, "Create an `f32` symbolic value");
+wrap!(f64, f64_symbol, () -> f64, "Create an `f64` symbolic value");
 
 #[doc = "Create a `u8` symbolic value"]
-pub fn u8_symbol() -> u8 {
-    i8_symbol() as u8
+pub fn u8() -> u8 {
+    i8() as u8
 }
 
 #[doc = "Create a `u32` symbolic value"]
-pub fn u32_symbol() -> u32 {
-    i32_symbol() as u32
+pub fn u32() -> u32 {
+    i32() as u32
 }
 
 #[doc = "Create a `u64` symbolic value"]
-pub fn u64_symbol() -> u64 {
-    i64_symbol() as u64
+pub fn u64() -> u64 {
+    i64() as u64
 }
